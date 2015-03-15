5688f3d753558f36d80956c73965a9552b220f7f
zalbo <sbabbari@gmail.com>
2015-03-15 16:48:30 +0100

    add comment

--- controller.rb
+++ controller.rb
@@ -8,23 +8,35 @@ require 'sqlite3'
 require 'logger'
 require 'pry'
 
+
 ActiveRecord::Base.logger = Logger.new(STDOUT)
 
 ActiveRecord::Base.establish_connection(
-  :adapter => 'sqlite3',
-  :database => 'zalbo.db'
+:adapter => 'sqlite3',
+:database => 'zalbo.db'
 )
 
 class CreateMessageMigration < ActiveRecord::Migration
   def change
     create_table :messages do |t|
       t.text :email
-      t.text :message
+      t.text :content
+    end
+  end
+end
+
+class CreateCommentMigration < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.integer :message_id
+      t.text :email
+      t.text :content
     end
   end
 end
 
 ActiveRecord::Migrator.migrate CreateMessageMigration
+ActiveRecord::Migrator.migrate CreateCommentMigration
 
 begin
   CreateMessageMigration.new.migrate(:up)
@@ -32,20 +44,39 @@ rescue ActiveRecord::StatementInvalid
   puts "table messages already exists"
 end
 
+begin
+  CreateCommentMigration.new.migrate(:up)
+rescue ActiveRecord::StatementInvalid
+  puts "table comments already exists"
+end
+
 class Message < ActiveRecord::Base
-  validates_presence_of :message
+  validates_presence_of :content
   validates :email, format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/ }
 
+  has_many :comments
+
   def self.search(word)
-    where("message LIKE ? OR email LIKE ? ", "%#{word}%" , "%#{word}%")
+    where("content LIKE ? OR email LIKE ? ", "%#{word}%" , "%#{word}%")
   end
 end
 
+class Comment < ActiveRecord::Base
+  validates_presence_of :content
+  validates :email, format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/ }
 
+  belongs_to :message
+end
+
+enable :sessions
 
 get '/' do
+  session[:search] = nil if params[:search] == ""
+  params[:search] = session[:search] if session[:search] && params[:search].nil?
+
   if params[:search]
     @messages = Message.search(params[:search])
+    session[:search] = params[:search]
   else
     @messages = Message.all
   end
@@ -53,18 +84,19 @@ get '/' do
 end
 
 get '/new' do
-  @errors = Message.new.errors
+  @message = Message.new
 
   erb :new
 end
 
 get '/show/:id' do
   @message = Message.find(params[:id].to_i)
+  @comment = Comment.new(message_id: @message.id)
+
   erb :show
 end
 
 get '/edit/:id' do
-  @errors = Message.new.errors.full_messages
   @message = Message.find(params[:id].to_i)
 
   erb :edit
@@ -77,24 +109,39 @@ get '/delete/:id' do
 end
 
 post '/' do
-  @messages = Message.all
+  @message = Message.new(params)
 
-  if Message.create({ :message => params[:message].strip , :email => params[:email]}).valid?
-    erb :index
+  if @message.save
+    redirect('/')
   else
-    @errors = Message.create({ :message => params[:message].strip , :email => params[:email]}).errors.full_messages
     erb :new
   end
 end
 
 post '/edit/:id' do
-  @messages = Message.all()
   @message = Message.find(params[:id].to_i)
 
-  if Message.update(params[:id].to_i, :message => params[:message].strip , :email => params[:email]).valid?
-    erb :index
+  if @message.update(message_params(params))
+    redirect('/')
   else
-    @errors = Message.update(params[:id].to_i, :message => params[:message].strip , :email => params[:email]).errors.full_messages
     erb :edit
   end
+end
+
+post '/comment' do
+
+  @message = Message.find(params[:message_id].to_i)
+  @comment = @message.comments.build
+
+  if @comment.update(message_params(params))
+    redirect("/show/#{@message.id}")
+  else
+    erb :show
+  end
+end
+
+def message_params(params)
+  params.delete("splat")
+  params.delete("capture")
+  params
 end
